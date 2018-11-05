dcf4639d79f22fa3e8c469ac33e2a2943394b6e3
Nikolay Gromov <nicothin@gmail.com>
2018-11-05 20:27:30 +0300

    html + js + fonts + images

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const dir =  {
+  src: './src/',
+  build: './build/',
+};
+
 const { series, parallel, src, dest, watch } = require('gulp');
 const plumber = require('gulp-plumber');
 const sass = require('gulp-sass');
@@ -8,9 +13,20 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
 const del = require('del');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+function html() {
+  return src(dir.src + '*.html')
+    .pipe(plumber())
+    .pipe(dest(dir.build));
+}
+exports.html = html;
 
 function styles() {
-  return src('./scss/style.scss')
+  return src(dir.src + 'scss/style.scss')
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass())
@@ -18,28 +34,73 @@ function styles() {
       autoprefixer({browsers: ['last 2 version']}),
     ]))
     .pipe(sourcemaps.write('/'))
-    .pipe(dest('./css/'))
+    .pipe(dest(dir.build + 'css/'))
     .pipe(browserSync.stream());
 }
 exports.styles = styles;
 
-function clean() {
-  return del('./css')
+function script() {
+  return gulp.src(dir.src + 'js/*.js')
+    .pipe(plumber())
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(concat('script.min.js'))
+    .pipe(gulp.dest(dir.build + 'js/'))
 }
+exports.script = script;
+
+function scriptsVendors() {
+  return gulp.src([
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/slick-carousel/slick/slick.min.js',
+      'node_modules/svg4everybody/dist/svg4everybody.min.js'
+    ])
+    .pipe(concat('vendors.min.js'))
+    .pipe(gulp.dest(dir.build + 'js/'))
+}
+
+function images() {
+  return gulp.src(dir.src + 'img/*.{jpg,jpeg,png,svg,webp,gif}')
+    .pipe(gulp.dest(dir.build + 'img/'));
+}
+exports.images = images;
+
+function fonts() {
+  return src(dir.src + 'fonts/*.{ttf,eot,svg,woff,woff2}')
+    .pipe(dest(dir.build + 'fonts/'));
+}
+exports.fonts = fonts;
+
+function clean() {
+  return del(dir.build)
+}
+exports.clean = clean;
 
 function serve() {
   browserSync.init({
-    server: './',
+    server: dir.build,
     startPath: 'index.html',
     open: false,
     port: 8080,
   });
   watch([
-    './scss/*.scss',
-    './scss/blocks/*.scss',
+    dir.src + 'scss/*.scss',
+    dir.src + 'scss/blocks/*.scss',
   ], { delay: 100 }, styles);
-  watch('./*.html').on('change', browserSync.reload);
-  watch('./js/*.js').on('change', browserSync.reload);
+  watch(dir.src + '*.html', html);
+  watch(dir.src + '*js/*.js', script);
+  watch(dir.src + 'img/*.{jpg,jpeg,png,svg,webp,gif}', images);
+  watch([
+    dir.build + '*.html',
+    dir.build + 'js/*.js',
+    dir.build + 'img/*.{jpg,jpeg,png,svg,webp,gif}',
+  ]).on('change', browserSync.reload);
 }
 
-exports.default = series(clean, styles, serve);
+exports.default = series(
+  clean,
+  parallel(styles, html, script, scriptsVendors, images, fonts),
+  serve
+);

b53158bee606aa6b43988d3b487721e72808c053
Nikolay Gromov <nicothin@gmail.com>
2020-03-01 00:57:16 +0300

    Добавил eslint

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
-// function ready(fn) {
-//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
-//   }
-// }
-//
-// ready(function(){
-//   console.log('DOM ready');
-// });
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(function(){
+  console.log('DOM ready');
+});
